662dfb259da09245dac4c624d9b5afe9e46f697c
aemanley <mej18008@byui.edu>
2022-05-21 15:49:13 -0600

    .

--- chamber/js/chamber.js
+++ chamber/js/chamber.js
@@ -7,3 +7,29 @@ const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
 	now
 );
 datefield.innerHTML = `<em>${fulldate}</em>`;
+
+
+
+
+function toggleMenu(){
+	document.getElementById('primarynav').classList.toggle("open");
+	document.getElementById('hamburgerbtn').classList.toggle("open");
+}
+
+const x= document.getElementById('hamburgerbtn')
+x.onclick= toggleMenu;
+
+
+
+/*
+function displaybanner(){
+	banner= documentgetElementById('banner')
+
+	const banner = new Date();
+const invitation= banner.getDay();
+
+if( invitation == 2 || invitation == 3){
+ banner.style.display='block'
+}
+}
+*/
