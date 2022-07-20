4129f651be5ea2c413d03647ae10b840e03f6a7b
aemanley <mej18008@byui.edu>
2022-07-19 20:29:53 -0600

    .

--- chamber/js/directory.js
+++ chamber/js/directory.js
@@ -9,18 +9,17 @@ async function getBusiness() {
     let data = await response.json();
     console.log(data);
     buildBusinessCards(data);
-  } 
- else {
+  } else {
     throw Error(response.statusText);
   }
 }
 
 function buildBusinessCards(data) {
-  data.business.forEach(business => {
-    let card =document.createElement('section');
-    let h2 = document.createElement('h2');
-    let p = document.createElement('p');
-let phone = document.createElement('p');
+  data.business.forEach((business) => {
+    let card = document.createElement("section");
+    let h2 = document.createElement("h2");
+    let p = document.createElement("p");
+    let phone = document.createElement("p");
     let url = document.createElement("a");
     let img = document.createElement("img");
     let level = document.createElement("p");
@@ -30,7 +29,7 @@ let phone = document.createElement('p');
     phone.innerHTML = `${business.phonenumber}`;
     url.innerHTML = `${business.websiteurl}`;
     img.setAttribute("src", business.imageurl);
-    level.innerHTML=` ${business.membershiplevel}`;
+    level.innerHTML = ` ${business.membershiplevel}`;
 
     card.append(h2);
     card.append(img);
@@ -39,35 +38,28 @@ let phone = document.createElement('p');
     cards.append(card);
     card.append(url);
     card.append(level);
-  
   });
 }
 
 getBusiness();
 
-
-
-
 //Last modified//
 document.querySelector(
-    "#updated"
-  ).textContent = `Last updated: ${document.lastModified}`;
-  
-  const datefield = document.querySelector(".date");
-  const now = new Date();
-  const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
-    now
-  );
-  datefield.innerHTML = `<em>${fulldate}</em>`;
+  "#updated"
+).textContent = `Last updated: ${document.lastModified}`;
 
+const datefield = document.querySelector(".date");
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+  now
+);
+datefield.innerHTML = `<em>${fulldate}</em>`;
 
-
-  //Hamburguer Button
+//Hamburguer Button
 function toggleMenu() {
-    document.getElementById("primarynav").classList.toggle("open");
-    document.getElementById("hamburgerbtn").classList.toggle("open");
-  }
-  
-  const x = document.getElementById("hamburgerbtn");
-  x.onclick = toggleMenu;
-  
+  document.getElementById("primarynav").classList.toggle("open");
+  document.getElementById("hamburgerbtn").classList.toggle("open");
+}
+
+const x = document.getElementById("hamburgerbtn");
+x.onclick = toggleMenu;
---
+++ finalproject/js/temple.js
@@ -0,0 +1,42 @@
+const requestURL = "js/temple.json";
+const cards = document.querySelector(".cards");
+
+async function getTemples() {
+  let response = await fetch(requestURL);
+  if (response.ok) {
+    let data = await response.json();
+    console.log(data);
+    buildTemplesCards(data);
+  } else {
+    throw Error(response.statusText);
+  }
+}
+
+function buildTemplesCards(data) {
+  data.temples.forEach((temples) => {
+    let card = document.createElement("section");
+    let h2 = document.createElement("h2");
+    let ad= document.createElement("p");
+    let ph=document.createElement("p");
+    let ser=document.createElement("p");
+    let ord=document.createElement("p");
+    let tcs=document.createElement("p");
+
+
+    h2.innerHTML = `${temples.name}`;
+    ad.innerHTML=`${temples.adress}`;
+    ph.innerHTML=`${temples.telephone}`;
+    ser.innerHTML=`${temples.services}`;
+    ord.innerHTML=`${temples.ordinanceschedule}`;
+    tcs.innerHTML=`${temples.templeclosureschedule}`;
+
+    card.append(h2);
+    cards.append(card);
+    cards.append(ad);
+    cards.append(ph);
+    cards.append(ser);
+    cards.append(ord);
+    cards.append(tcs);
+  });
+}
+getTemples();
