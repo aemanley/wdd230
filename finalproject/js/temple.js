83c83b60c7720fab18d7deaaba773f40af6b2c7b
aemanley <mej18008@byui.edu>
2022-07-20 09:09:17 -0600

    .

--- finalproject/js/temple.js
+++ finalproject/js/temple.js
@@ -16,19 +16,21 @@ function buildTemplesCards(data) {
   data.temples.forEach((temples) => {
     let card = document.createElement("section");
     let h2 = document.createElement("h2");
-    let ad= document.createElement("p");
-    let ph=document.createElement("p");
-    let ser=document.createElement("p");
-    let ord=document.createElement("p");
-    let tcs=document.createElement("p");
-
+    let ad = document.createElement("p");
+    let ph = document.createElement("p");
+    let ser = document.createElement("p");
+    let ord = document.createElement("p");
+    let tcs = document.createElement("p");
+    let img = document.createElement("img");
 
     h2.innerHTML = `${temples.name}`;
-    ad.innerHTML=`${temples.adress}`;
-    ph.innerHTML=`${temples.telephone}`;
-    ser.innerHTML=`${temples.services}`;
-    ord.innerHTML=`${temples.ordinanceschedule}`;
-    tcs.innerHTML=`${temples.templeclosureschedule}`;
+    ad.innerHTML = `${temples.adress}`;
+    ph.innerHTML = `${temples.telephone}`;
+    ser.innerHTML = `${temples.services}`;
+    ord.innerHTML = `${temples.ordinanceschedule}`;
+    tcs.innerHTML = `${temples.templeclosureschedule}`;
+    img.setAttribute("src", temples.image);
+   
 
     card.append(h2);
     cards.append(card);
@@ -37,6 +39,7 @@ function buildTemplesCards(data) {
     cards.append(ser);
     cards.append(ord);
     cards.append(tcs);
+    cards.append(img);
   });
 }
 getTemples();
